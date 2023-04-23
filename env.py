fc8ffd63bf0e1d05202e24dcdd68f217c7b1dac7
MalikGit <97935254+Virtualunionsex@users.noreply.github.com>
2023-04-24 00:55:27 +0700

    Update env.py

--- env.py
+++ env.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = os.getenv("API_ID", "12861350").strip()
-API_HASH = os.getenv("API_HASH", "e1429ae7374edc95c986d652df783bf7").strip()
-BOT_TOKEN = os.getenv("BOT_TOKEN", "5619588117:AAGQVPs6LY3VpToYsORw49NbrpFtYl62Md8").strip()
-DATABASE_URL = os.getenv("DATABASE_URL", "postgres://yxghfmci:QkCv1t2hzOzHZz9J-my1xx9En3QgUvwC@ruby.db.elephantsql.com/yxghfmci").strip()
-MUST_JOIN = os.getenv("MUST_JOIN", "https://t.me/zennihhh")
+API_ID = os.getenv("API_ID", "16246834").strip()
+API_HASH = os.getenv("API_HASH", "29b3ffa9245c07f05375b92f38e8f13d").strip()
+BOT_TOKEN = os.getenv("BOT_TOKEN", "5940961389:AAGD32HWrBCAEHF-qfzcXyoG_Wdd6P7fbGA").strip()
+DATABASE_URL = os.getenv("DATABASE_URL", "postgres://dqvpoxxi:4McOyOwZaE3bQI5FSoydXS81pt9gzsIx@rosie.db.elephantsql.com/dqvpoxxi").strip()
+MUST_JOIN = os.getenv("MUST_JOIN", "https://t.me/nakama_asl")
 
 if not API_ID:
     print("No API_ID found. Exiting...")
